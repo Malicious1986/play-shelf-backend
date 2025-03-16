53d2992f06d75bebc443121a373f7819f360d0e1
Osadchyi Yurii <malicious1986@gmail.com>
2025-03-16 21:44:29 +0000

    added aws cors

--- config/cors.js
+++ config/cors.js
@@ -1,4 +1,9 @@
-const allowedOrigins = ["http://localhost:5173", "https://play-shelf.vercel.app", "https://studio.apollographql.com/sandbox/explorer"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://play-shelf.vercel.app",
+  "https://studio.apollographql.com/sandbox/explorer",
+  "https://main.d19t645w93y4bm.amplifyapp.com",
+];
 
 export const corsOptions = {
   origin: (origin, callback) => {
